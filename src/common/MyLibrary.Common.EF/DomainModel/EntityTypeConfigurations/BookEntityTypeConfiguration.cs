443c829415bc2f115d1c6ee7a80bb7336dc44d24
Dmitry Barabash <dmitry@barabash.com>
2025-11-17 12:39:40 +0000

    Refactor: Extract EF Core entities' configurations into separate classes

---
+++ src/common/MyLibrary.Common.EF/DomainModel/EntityTypeConfigurations/BookEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyLibrary.Common.DomainModel;
+
+namespace MyLibrary.Common.EF.DomainModel.EntityTypeConfigurations;
+
+internal class BookEntityTypeConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        // Устанавливаем автоинкремент для первичного ключа
+        builder.Property(b => b.Id).ValueGeneratedOnAdd();
+
+        // Устанавливаем длину строкового поля и характеристику NOT NULL
+        builder.Property(b => b.Name).HasMaxLength(100);
+
+        builder.Property(b => b.Name).IsRequired();
+
+        // Указываем точность для поля типа decimal
+        builder.Property(b => b.Price).HasColumnType("decimal(18,2)");
+    }
+}
--- src/common/MyLibrary.Common.EF/DomainModel/MyLibraryContext.cs
+++ src/common/MyLibrary.Common.EF/DomainModel/MyLibraryContext.cs
@@ -19,52 +19,10 @@ public class MyLibraryContext : DbContext
     // которые выходят за дефолтные установки Entity Framework
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //
-        // Authors
-        //
-
-        // Устанавливаем автоинкремент для первичного ключа
-        modelBuilder.Entity<Author>().Property(a => a.Id).ValueGeneratedOnAdd();
-        // Устанавливаем длину строкового поля и характеристику NOT NULL
-        modelBuilder.Entity<Author>().Property(a => a.Name).HasMaxLength(50);
-        modelBuilder.Entity<Author>().Property(a => a.Name).IsRequired();
-        // Устанавливаем ссылочную целостность для связи "один-ко-многим"
-        // между таблицами Authors и Books и внешний ключ в таблице Books
-        modelBuilder.Entity<Author>()
-            .HasMany(a => a.Books)
-            .WithOne(b => b.Author)
-            .HasForeignKey(b => b.AuthorFk)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        //
-        // Presses
-        //
-
-        // Устанавливаем автоинкремент для первичного ключа
-        modelBuilder.Entity<Press>().Property(p => p.Id).ValueGeneratedOnAdd();
-        // Устанавливаем длину строкового поля и характеристику NOT NULL
-        modelBuilder.Entity<Press>().Property(p => p.Name).HasMaxLength(50);
-        modelBuilder.Entity<Press>().Property(p => p.Name).IsRequired();
-        // Устанавливаем ссылочную целостность для связи "один-ко-многим"
-        // между таблицами Presses и Books и внешний ключ в таблице Books
-        modelBuilder.Entity<Press>()
-            .HasMany(p => p.Books)
-            .WithOne(b => b.Press)
-            .HasForeignKey(b => b.PressFk)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        //
-        // Books
-        //
-
-        // Устанавливаем автоинкремент для первичного ключа
-        modelBuilder.Entity<Book>().Property(b => b.Id).ValueGeneratedOnAdd();
-        // Устанавливаем длину строкового поля и характеристику NOT NULL
-        modelBuilder.Entity<Book>().Property(b => b.Name).HasMaxLength(100);
-        modelBuilder.Entity<Book>().Property(b => b.Name).IsRequired();
-        // Указываем точность для поля типа decimal
-        modelBuilder.Entity<Book>().Property(b => b.Price).HasColumnType("decimal(18,2)");
-
+        // Задаем правила сопоставления классов модели с таблицами БД с помощью Fluent API.
+        // Чтобы не загромождать метод OnModelCreating, используем раздельные реализации
+        // интерфейса IEntityTypeConfiguration<Entity> для каждой таблицы.
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyLibraryContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
     }
